6f50ec9e73b99d4ab0a2e1e18599e40e16fc7edc
Gabriel Batres <gbatres02@gmail.com>
2023-12-23 09:45:00 -0600

    Primer versión del servidor de autenticación

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# api-auth
-API para autenticación con token
+# API Autenticación
+
+API para generar la autenticación token algoritmo SHA-256
+
+###  Requisitos
+
+Para construir esta aplicación se requieren como versiones mínimas:
+
+* Java 17
+* Maven 3.2.0
+
+El puerto de despliegue es el default
+
+```
+8080
+```
+
+### Variables de entorno
+
+* AUTH_CLIENT_ID: URI de conexión a Mongo DB (mongodb://localhost:27017)
+* AUTH_CLIENT_PASSWORD: Nombe de la DB en mongo
+* AUTH_REDIRECT_URI: Porcentaje de Valuación (Por defecto es 80)
+* AUTH_USER_NAME: URI de conexión a Mongo DB (mongodb://localhost:27017)
+* AUTH_USER_PASSWORD: Nombe de la DB en mongo
+* AUTH_USER_ROL: Porcentaje de Valuación (Por defecto es 80)
---
+++ src/main/java/com/nmp/auth/configuration/AuthServerConfig.java
@@ -0,0 +1,167 @@
+package com.nmp.auth.configuration;
+
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.util.UUID;
+
+/**
+ * Bean de configuración para validar y generar el token
+ * @author Gabriel Batres
+ */
+@Configuration
+public class AuthServerConfig {
+
+    private static final String CLIENT_ID = "client";
+
+    private static final String SECRET = "{noop}secret";
+
+    @Value("${auth.server.redirect}")
+    private String redirectUri;
+
+    @Value("${auth.server.user.name}")
+    private String username;
+
+    @Value("${auth.server.user.password}")
+    private String userPassword;
+
+    @Value("${auth.server.issuer.uri}")
+    private String issuerUri;
+
+
+
+    /**
+     * Configuración de los filtros de seguridad para la autenticación.
+     */
+    @Bean
+    @Order(1)
+    public SecurityFilterChain authSecurityFilterChain(HttpSecurity http) throws Exception {
+        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
+        http.getConfigurer(OAuth2AuthorizationServerConfigurer.class).oidc(Customizer.withDefaults());
+        http.exceptionHandling(exception -> exception
+                .authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login")))
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+        return http.build();
+    }
+
+    /**
+     * Configuración de la cadena de filtros de seguridad para las solicitudes web.
+     */
+    @Bean
+    @Order(2)
+    public SecurityFilterChain webSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults());
+        return http.build();
+    }
+
+    /**
+     * Información del usuario para la autorizacion
+     */
+    @Bean
+    UserDetailsService users() {
+        UserDetails user = User.withUsername(username)
+                .password("{noop}"+userPassword)
+                .authorities("ROLE_USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    /**
+     * Configuración del repositorio de clientes.
+     */
+    @Bean
+    public RegisteredClientRepository registeredClientRepository() {
+        RegisteredClient registeredClient = RegisteredClient.withId(UUID.randomUUID().toString())
+                .clientId(CLIENT_ID)
+                .clientSecret(SECRET)
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+                .redirectUri(redirectUri)
+                .scope(OidcScopes.OPENID)
+                .clientSettings(clientSettings())
+                .build();
+        return new InMemoryRegisteredClientRepository(registeredClient);
+    }
+
+    @Bean
+    public ClientSettings clientSettings() {
+        return ClientSettings.builder().requireProofKey(true).build();
+    }
+
+    @Bean
+    public AuthorizationServerSettings authorizationServerSettings() {
+        return AuthorizationServerSettings.builder().issuer(issuerUri).build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder(JWKSource<SecurityContext> jwkSource) {
+        return OAuth2AuthorizationServerConfiguration.jwtDecoder(jwkSource);
+    }
+
+    @Bean
+    public JWKSource<SecurityContext> jwkSource() {
+        RSAKey rsaKey = generateRSAKey();
+        JWKSet jwkSet = new JWKSet(rsaKey);
+        return (jwkSelector, securityContext) -> jwkSelector.select(jwkSet);
+    }
+
+    /**
+     * Genera la clave RSA
+     */
+    private RSAKey generateRSAKey() {
+        KeyPair keyPair = generateKeyPair();
+        RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+        RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+        return new RSAKey.Builder(publicKey).privateKey(privateKey).keyID(UUID.randomUUID().toString()).build();
+    }
+
+    /**
+     * Genera las claves RSA para su uso JWK.
+     */
+    private KeyPair generateKeyPair() {
+        KeyPair keyPair;
+        try {
+            KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
+            generator.initialize(2048);
+            keyPair = generator.generateKeyPair();
+        }catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+
+        return keyPair;
+    }
+
+}
